fd83ec79519abfa05dc72750e75827a85d1bf166
kritinika <kirtisharma8822103020@gmail.com>
2025-07-01 13:46:12 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (87.16%) - LeetSync

---
+++ 367-valid-perfect-square/valid-perfect-square.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num){
+        int n =sqrt(num);
+        if(n*n==num)return 1;
+        return 0;
+    
+
+        
+    
+        
+
+        
+    }
+};
