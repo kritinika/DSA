65fccecb3b9f50d4b636339916a8dc484758e927
kritinika <kirtisharma8822103020@gmail.com>
2025-07-12 00:16:25 +0530

    Time: 23 ms (30.61%) | Memory: 26.3 MB (8.24%) - LeetSync

--- 978-valid-mountain-array/valid-mountain-array.cpp
+++ 978-valid-mountain-array/valid-mountain-array.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     bool validMountainArray(vector<int>& arr){
-        if(arr.size()<=2||arr[0]>arr[1])return false;
-        int i =1;
-        while(i<arr.size()-1&&arr[i]>arr[i-1])i++;
-         while(i<arr.size()&&arr[i]<arr[i-1])i++;
-
-      
-        return i==arr.size();
+        int n=arr.size();
+        if(n<=2||arr[0]>arr[1])return false;
+        int i=0,j=n-1;
+        while(i<n&&arr[i]<arr[i+1])i++;
+        while(j>0&&arr[j-1]>arr[j])j--;
+        return i>0&&i==j&&j<n-1;
         
     }
 };
