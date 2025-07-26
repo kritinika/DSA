31f3d95701c71160e3d15f69d0b772bc1289eb99
kritinika <kirtisharma8822103020@gmail.com>
2025-07-26 21:37:42 +0530

    Time: 0 ms (100.00%) | Memory: 29.8 MB (11.88%) - LeetSync

--- 3788-maximum-unique-subarray-sum-after-deletion/maximum-unique-subarray-sum-after-deletion.cpp
+++ 3788-maximum-unique-subarray-sum-after-deletion/maximum-unique-subarray-sum-after-deletion.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int maxSum(vector<int>& nums){
-     vector<int>arr(101,-1);
+        unordered_set<int>arr;
         int sum=0;
         int maxneg =INT_MIN;
         for(auto &it:nums){
             if(it<=0)maxneg=max(maxneg,it);
-            else if(arr[it]==-1){ sum+=it;
-                    arr[it]=1;    
+            else {
+                arr.insert(it);
             }
-
         }
+        for(auto it:arr)sum+=it;
         return sum == 0 ? maxneg :sum;
         
     }
