55b9287e3f4f383138dd5e3f9cae4b9e094917e3
kritinika <kirtisharma8822103020@gmail.com>
2025-07-29 22:56:18 +0530

    Time: 14 ms (42.14%) | Memory: 31.6 MB (36.54%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
@@ -1,22 +1,48 @@
+// class Solution {
+// public:
+//     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid){
+//         int n=grid.size();
+//         int f=0,s=0;
+//         int l=n*n;
+//         vector<int>arr(l+1,0);
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<n;j++){
+//                 arr[grid[i][j]]++;
+//             }
+//         }
+        
+//        for(int i=0;i<arr.size();i++){
+//         if(arr[i]==2) f=i;
+//         if(arr[i]==0)s=i;
+        
+//         }
+// return {f,s};
+        
+    
+// };
 class Solution {
 public:
     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid){
-        int n=grid.size();
-        int f=0,s=0;
-        int l=n*n;
-        vector<int>arr(l+1,0);
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                arr[grid[i][j]]++;
-            }
+    int n=grid.size();
+    int f=0,s=0;
+    unordered_map<int,int>mp;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            mp[grid[i][j]]++;
         }
-        
-       for(int i=0;i<arr.size();i++){
-        if(arr[i]==2) f=i;
-        if(arr[i]==0)s=i;
-        
-        }
-return {f,s};
-        
+    }
+    
+    int xorr =0 ,p=1;
+    for(auto&[a,b]:mp){
+        if(b==2)f=a;
+        xorr^=a;
+        xorr^=p;
+        p++;
+
+    }
+    xorr^=p;
+    
+
+      return {f,xorr};  
     }
 };
