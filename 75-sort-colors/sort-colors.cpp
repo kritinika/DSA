a23077a2002ef31e7a360b6fde7b168e291ba338
kritinika <kirtisharma8822103020@gmail.com>
2025-07-30 17:06:04 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (78.93%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,17 +1,24 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums){
-        vector<int>k(3,0);
-        for(auto it:nums)k[it]++;
+        int n=nums.size();
+        vector<int>arr(3,0);
+        for(auto it:nums)arr[it]++;
         nums.clear();
         int i=0;
         while(i<3){
-            while(k[i]!=0){
-                nums.push_back(i);
-                k[i]--;
-            }
-            i++;
+        while(arr[i]!=0){
+            nums.push_back(i);
+            arr[i]--;
         }
-        return ;  
+            i++;
+        
+
+    }
+      
+        
+
+
+        
     }
 };
