dd1beb072301d415e439b704f54b959eca4d96df
Kirti Sharma <kirtisharma8822103020@gmail.com>
2025-08-22 22:02:20 +0530

    Time: 266 ms (36.10%) | Memory: 133.6 MB (38.34%) - LeetSync

--- 3461-find-the-minimum-area-to-cover-all-ones-i/find-the-minimum-area-to-cover-all-ones-i.cpp
+++ 3461-find-the-minimum-area-to-cover-all-ones-i/find-the-minimum-area-to-cover-all-ones-i.cpp
@@ -5,14 +5,14 @@ public:
         for(int i=1;i<=grid.size();i++){
             for(int j=1;j<=grid[0].size();j++){
                 if(grid[i-1][j-1]==1){
-                    l=min(l,j);
+                    l=min(l,j-1);
                     r=max(r,j);
-                    u=min(u,i);
+                    u=min(u,i-1);
                     d=max(d,i);
                 }
             }
         }
-        return ((r-l+1)*(d-u+1));
+        return ((r-l)*(d-u));
         
     }
 };
